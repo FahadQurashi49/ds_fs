a968704cea1effb312a50067c997e4e20d0a4bb1
Fahad Qureshi <fahad.ahmad049@gmail.com>
2018-11-18 23:33:28 +0500

    changes in config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 class Config {
     public servers = [
-        "http://192.168.150.128:3000/"
+        "http://192.168.150.128:3000/",
+        "http://192.168.1.6:3000/"
     ];
     public dir = './fs';
     public isMaster = false;
